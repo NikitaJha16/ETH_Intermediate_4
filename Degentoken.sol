cfbafcca53fc128c322a8a3b14bc17bf7c6de2c0
NikitaJha16 <139324050+NikitaJha16@users.noreply.github.com>
2024-08-22 18:07:58 +0530

    Update Degentoken.sol

--- Degentoken.sol
+++ Degentoken.sol
@@ -7,11 +7,17 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract DegenToken is ERC20, Ownable, ERC20Burnable 
 {
-    constructor() ERC20("Degen", "DGN") Ownable(msg.sender){}
-
     mapping(address => uint256) private _rewards;
+    mapping(string => uint256) private _itemCosts;  // Mapping to store item costs
 
-
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) 
+    {
+        // Initialize items with their costs
+        _itemCosts["Sword"] = 100;  
+        _itemCosts["Shield"] = 150; 
+        _itemCosts["Potion"] = 50;  
+        _itemCosts["Armor"] = 200;  
+    }
 
     // Function to mint new tokens
     function mint(address to, uint256 amount) external onlyOwner 
@@ -45,4 +51,28 @@ contract DegenToken is ERC20, Ownable, ERC20Burnable
     function checkReward(address account) external view returns (uint256) 
     {
         return _rewards[account];
-    }}
+    }
+
+    // Function to set item costs
+    function setItemCost(string calldata itemName, uint256 cost) external onlyOwner 
+    {
+        _itemCosts[itemName] = cost;
+    }
+
+    // Function to get item cost
+    function getItemCost(string calldata itemName) external view returns (uint256) 
+    {
+        return _itemCosts[itemName];
+    }
+
+    // Function to redeem an item
+    function redeem(string calldata itemName) external 
+    {
+        uint256 cost = _itemCosts[itemName];
+        require(cost > 0, "Item does not exist");
+        require(balanceOf(msg.sender) >= cost, "Insufficient balance to redeem");
+
+        _burn(msg.sender, cost);
+
+    }
+}
